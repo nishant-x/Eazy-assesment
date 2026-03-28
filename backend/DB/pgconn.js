348d9721ea274a55169f5e17651a4a073370dc1e
Nishant Jhade <jhadenishant@gmail.com>
2026-03-28 15:29:23 +0530

    deployement changes

--- backend/DB/pgconn.js
+++ backend/DB/pgconn.js
@@ -1,19 +1,12 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  host: 'db.geigaidqjzwafwdxunpk.supabase.co',
-  port: 5432,
-  // Force IPv4
-  family: 4
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.connect()
   .then(() => console.log("Database Connected ✅"))
-  .catch((err) => console.error("Database Connection Error ❌", err));
+  .catch(err => console.error("Database Connection Error ❌", err));
 
-module.exports = {
-    pool
-};
+module.exports = { pool };
